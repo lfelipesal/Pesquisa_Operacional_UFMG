26e89c77fcdc2c6f83d21217c651b00e33f02170
luiz <luiz49138@gmail.com>
2025-01-12 18:40:59 -0300

    Adicionando Restrições e altualizando custo

--- simulacao/main.py
+++ simulacao/main.py
@@ -14,20 +14,31 @@ ativos: List[Ativo] = busca_dados_financeiros()
 
 # Parâmetros do problema
 num_ativos = len(ativos)
-mu = []
-C = []
-for ativo in ativos:
-    mu.append(ativo.retorno)
-    C.append(ativo.valor)
+custo_fixo = 10  # Custo fixo por transação
+spread_fixo = 0.005  # Spread fixo de 0.5%
+taxa_operacional = 5  # Custo diário fixo
+N_maximo = 10 # Número máximo de ativos
+B_max = 3 # Custo maximo gasto em transação
+W_min = 0.02 # Alocação mínima de 2% em cada ativo
+Volat_max =  0.05 # Definindo a volatilidade máxima dos ativos sendo 5%
+desvio_padrao = [] # Defininado o desvio padrão individual (Volatilidade) de cada ativo
+limite_liquidez = [] # Definir um limite (1%-5% do volume médio diário)
+mu = []     # Definindo os retornos esperado de cada ativo com base nos dividendos
+C = []      # Definindo os custos das transações de cada ativo com base no seu ultimo valor sobre um spread fixo
 
-model = gp.Model("FuncaoObjetivo")
+for ativo in ativos:
+    desvio_padrao.append(ativo.desvio_padrao)
+    limite_liquidez.append(0.01 * ativo.volume_medio)
+    mu.append(ativo.retorno)
+    C.append( custo_fixo +  (spread_fixo * ativo.valor) + taxa_operacional)
+
 
 
 # Criar um modelo de otimização
 model = gp.Model("Modelo_Com_Funcao_Objetivo")
 
 # Variáveis do problema
-w = model.addVars(num_ativos, vtype=GRB.CONTINUOUS, name="w")
+w = model.addVars(num_ativos, vtype=GRB.CONTINUOUS, name="w") #
 x = model.addVars(num_ativos, vtype=GRB.BINARY, name="x")
 
 # Objetivo: Z = sum(mu[i] * w[i] - C[i] * x[i] for i in range(n))
@@ -44,7 +55,14 @@ model.setObjective(
     GRB.MAXIMIZE)
 
 # Restrições
-model.addConstr(gp.quicksum(x[i] for i in range(num_ativos)) >= 4, name="restricao_soma_xi")
+model.addConstr(gp.quicksum(w[i] for i in range(num_ativos)) <= 1, name="Total_Investido")                              # 1- Orçamento Total por Investidor
+model.addConstr(gp.quicksum(x[i] for i in range(num_ativos)) >= 4, name="restricao_soma_xi")                            # 2- Número Mínimo de Investimentos
+model.addConstr(gp.quicksum(x[i] for i in range(num_ativos)) <= N_maximo, name="restricao_Max_ativos")                  # 3- Número Máximo de Ativos por Investidor
+model.addConstr((w[i] <= limite_liquidez[i]*x[i] for i in range(num_ativos)), name="limite_liquidez")                   # 4- Limites de Alocação por Ativo
+model.addConstr(gp.quicksum(C[i]*x[i] for i in range(num_ativos)) <= B_max, name="Custos_maximo")                       # 5- Custos de Transação
+model.addConstr((w[i] >= W_min*x[i] for i in range(num_ativos)), name="Alocacao_minima")                                # 6- Alocação Mínima por Ativo Selecionado
+model.addConstr(gp.quicksum(desvio_padrao[i]*x[i] for i in range(num_ativos)) <= Volat_max, name="Volatilidade_maxima") # 7- Volatilidade Máxima dos investidores    
+
 
 # Resolver o modelo
 model.optimize()
--- simulacao/src/ativo.py
+++ simulacao/src/ativo.py
@@ -1,4 +1,6 @@
 class Ativo:
-    def __init__(self, valor: float, retorno: float):
+    def __init__(self, valor: float, retorno: float, volume_medio: float, desvio_padrao: float):
         self.valor = valor
         self.retorno = retorno
+        self.volume_medio = volume_medio
+        self.desvio_padrao = desvio_padrao
